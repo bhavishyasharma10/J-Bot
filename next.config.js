c460cda545924c167b5ea0e454ad541b3c97fa4e
Bhavishya Sharma <48627889+bhavishyasharma10@users.noreply.github.com>
2025-03-29 20:53:24 +0530

    Add Webpack alias for easier module resolution

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
+const path = require("path");
+
 module.exports = {
-  env: {
-    TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
-    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
-    PORT: process.env.PORT,
-    LOG_LEVEL: process.env.LOG_LEVEL,
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
   },
 };
